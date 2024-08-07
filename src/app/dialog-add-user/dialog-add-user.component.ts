5269812ce2bae39b1a95b125acd485f81c261154
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-07 12:42:49 +0200

    added header & sidebar

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog, MatDialogActions, MatDialogContent, MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCalendar, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [
+    MatDialogActions,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatDialogContent,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+  ],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss',
+})
+export class DialogAddUserComponent {}
